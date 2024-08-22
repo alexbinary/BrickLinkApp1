69603d96700a1a799d13c947a8b551b26baae3a0
Alexandre Bintz <alexandre@bintz.io>
2024-08-22 20:25:51 +0200

    static order list

--- BrickLinkApp1/ContentView.swift
+++ BrickLinkApp1/ContentView.swift
@@ -5,14 +5,20 @@ import SwiftUI
 struct Order: Identifiable {
     
     let id: String
+    let date: String
+    let buyer: String
+    let items: Int
+    let lots: Int
+    let grandTotal: Int
+    let status: String
 }
 
 
 struct ContentView: View {
     
     let orders: [Order] = [
-        Order(id: "#1"),
-        Order(id: "#2"),
+        Order(id: "#1", date: "Aug 14, 2024", buyer: "legofan_fr", items: 248, lots: 71, grandTotal: 1681, status: "COMPLETED"),
+        Order(id: "#2", date: "Aug 14, 2024", buyer: "legofan_fr", items: 248, lots: 71, grandTotal: 1681, status: "COMPLETED"),
     ]
     
     var body: some View {
@@ -28,6 +34,15 @@ struct ContentView: View {
             Table(orders) {
                 
                 TableColumn("ID", value: \.id)
+                TableColumn("Date", value: \.date)
+                TableColumn("Buyer", value: \.buyer)
+                TableColumn("Items (lots)") { order in
+                    Text(verbatim: "\(order.items) (\(order.lots))")
+                }
+                TableColumn("Grand total") { order in
+                    Text(verbatim: "\(Double(order.grandTotal)/100) €")
+                }
+                TableColumn("Status", value: \.status)
                 
             }
                 .navigationTitle("Orders")
