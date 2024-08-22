7c6fa96c322289e695e727eb04f7f3d48ed8e4a7
Alexandre Bintz <alexandre@bintz.io>
2024-08-22 19:45:48 +0200

    Initial Commit

---
+++ BrickLinkApp1/BrickLinkApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  BrickLinkApp1App.swift
+//  BrickLinkApp1
+//
+//  Created by Alexandre Bintz on 22/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BrickLinkApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
