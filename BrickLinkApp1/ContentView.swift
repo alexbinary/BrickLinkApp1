c9351ae57c6a067d2f4fe7af66e1813bb7b73b76
Alexandre Bintz <alexandre@bintz.io>
2024-08-22 20:20:16 +0200

    setup orders list

--- BrickLinkApp1/ContentView.swift
+++ BrickLinkApp1/ContentView.swift
@@ -2,18 +2,35 @@
 import SwiftUI
 
 
+struct Order: Identifiable {
+    
+    let id: String
+}
+
+
 struct ContentView: View {
+    
+    let orders: [Order] = [
+        Order(id: "#1"),
+        Order(id: "#2"),
+    ]
     
     var body: some View {
         
         NavigationSplitView {
             
-            Text("Sidebar")
+            List {
+                Label("Orders", systemImage: "list.bullet")
+            }
             
         } detail: {
             
-            Text("Content")
-                .navigationTitle("Content")
+            Table(orders) {
+                
+                TableColumn("ID", value: \.id)
+                
+            }
+                .navigationTitle("Orders")
         }
     }
 }
