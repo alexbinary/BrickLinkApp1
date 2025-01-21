76b82dfd9c1db480d0b864f7866ac4472eca7e45
Alexandre Bintz <alexandre@bintz.io>
2025-01-21 08:59:19 +0100

    picking: reload inventories on appear

--- App1/Src/Orders/OrderPickingView.swift
+++ App1/Src/Orders/OrderPickingView.swift
@@ -69,6 +69,11 @@ struct OrderPickingView: View {
                 ])
             }
         }
+        .onAppear {
+            Task {
+                await appController.reloadInventories()
+            }
+        }
     }
     
     
