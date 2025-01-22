ac6d2fb148fd047ad073e06722d9e234ccbfd027
Alexandre Bintz <alexandre@bintz.io>
2025-01-22 08:07:15 +0100

    extract UploadedItemsView

--- App1/Src/Upload/UploadContentView.swift
+++ App1/Src/Upload/UploadContentView.swift
@@ -59,14 +59,7 @@ struct UploadContentView: View {
             }
             
             ScrollView {
-                
-                LazyVStack(alignment: .leading, spacing: 12, pinnedViews: .sectionHeaders) {
-                    
-                    let uploadedItems = appController.uploadedItems
-                        .sorted { $0.uploadDate > $1.uploadDate }
-                    
-                    section(header: "􀐫 Latest uploads", items: uploadedItems)
-                }
+                UploadedItemsView()
             }
             .tabItem {
                 Text("􀐫 History")
--- App1/Src/Upload/UploadedItemsView.swift
+++ App1/Src/Upload/UploadedItemsView.swift
@@ -3,110 +3,20 @@ import SwiftUI
 
 
 
-struct UploadContentView: View {
+struct UploadedItemsView: View {
     
     
     @EnvironmentObject var appController: AppController
-
-    @State var highlightedItemId: UploadItem.ID?
-    @State var selectedItemId: UploadItem.ID?
-    
-    @State var addViewVisible: Bool = false
     
     
     var body: some View {
      
-        TabView {
+        LazyVStack(alignment: .leading, spacing: 12, pinnedViews: .sectionHeaders) {
             
-            VStack {
-                
-                if addViewVisible {
-                    
-                    UploadAddView()
-                        .padding()
-                }
-                
-                ScrollView {
-                    
-                    LazyVStack(alignment: .leading, spacing: 12, pinnedViews: .sectionHeaders) {
-                        
-                        let uploadItems = appController.uploadItems
-                        
-                            .sorted { item1, item2 in
-                                
-                                let rem1 = appController.inventory(for: item1)?.remarks ?? appController.inventories(forAllColorsOf: item1).map { $0.remarks }.sorted().first
-                                let rem2 = appController.inventory(for: item2)?.remarks ?? appController.inventories(forAllColorsOf: item2).map { $0.remarks }.sorted().first
-                                
-                                switch (rem1, rem2) {
-                                    
-                                case (nil, nil):
-                                    return true
-                                    
-                                case (.some, nil):
-                                    return true
-                                    
-                                case (nil, .some):
-                                    return false
-                                    
-                                case (.some(let rem1), .some(let rem2)):
-                                    return rem1 < rem2
-                                }
-                            }
-                        
-                        section(header: "􀋲 Items to upload", items: uploadItems)
-                    }
-                }
-            }
-            .tabItem {
-                Text("􀋲 Upload")
-            }
+            let uploadedItems = appController.uploadedItems
+                .sorted { $0.uploadDate > $1.uploadDate }
             
-            ScrollView {
-                
-                LazyVStack(alignment: .leading, spacing: 12, pinnedViews: .sectionHeaders) {
-                    
-                    let uploadedItems = appController.uploadedItems
-                        .sorted { $0.uploadDate > $1.uploadDate }
-                    
-                    section(header: "􀐫 Latest uploads", items: uploadedItems)
-                }
-            }
-            .tabItem {
-                Text("􀐫 History")
-            }
-        }
-        .navigationTitle("Upload")
-        .toolbar {
-            Button {
-                addViewVisible.toggle()
-            } label: {
-                Text("􀅼").padding(.horizontal)
-            }
-        }
-        .onAppear {
-            Task {
-                await appController.reloadInventories()
-            }
-        }
-    }
-    
-    
-    @ViewBuilder
-    func section(header: String, items: [UploadItem]) -> some View {
-        
-        Section {
-            
-            ForEach(items) { item in
-                
-                UploadItemView(uploadItem: item)
-                    .padding([.leading, .trailing])
-            }
-            
-            Color.clear.frame(width: 0, height: 24)
-            
-        } header: {
-            
-            headerView(header, secondaryText: "\(items.count) items")
+            section(header: "􀐫 Latest uploads", items: uploadedItems)
         }
     }
     
