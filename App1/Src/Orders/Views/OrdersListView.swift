748fd7763776edbc31bd7cd849359dc621d59ac4
Alexandre Bintz <alexandre@bintz.io>
2025-02-21 14:47:25 +0100

    orders list: rewrite sections definition

--- App1/Src/Orders/Views/OrdersListView.swift
+++ App1/Src/Orders/Views/OrdersListView.swift
@@ -23,69 +23,60 @@ struct OrdersListView: View {
             
             LazyVStack(alignment: .leading, spacing: 12, pinnedViews: .sectionHeaders) {
                 
-                let sections: [(label: String, orders: [OrderSummary])] = [
-                    (
-                        label: "􁁿 In transit for 30+ days",
-                        orders: allOrders
-                            .filter { appController.orderBusinessStatus($0.id) == .inTransit && appController.orderChecklistUnchangedFor30Days($0.id) }
-                            .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
-                    ),
-                    (
-                        label: OrderBusinessStatus.giveFeedback.descriptionWithPicto,
-                        orders: allOrders
-                            .filter { appController.orderBusinessStatus($0.id) == .giveFeedback }
-                            .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
-                    ),
-                    (
-                        label: OrderBusinessStatus.validatePayment.descriptionWithPicto,
-                        orders: allOrders
-                            .filter { appController.orderBusinessStatus($0.id) == .validatePayment }
-                            .sorted { $0.date > $1.date }
-                    ),
-                    (
-                        label: OrderBusinessStatus.pickAndPack.descriptionWithPicto,
-                        orders: allOrders
-                            .filter { appController.orderBusinessStatus($0.id) == .pickAndPack }
-                            .sorted { $0.lots < $1.lots }
-                    ),
-                    (
-                        label: OrderBusinessStatus.ship.descriptionWithPicto,
-                        orders: allOrders
-                            .filter { appController.orderBusinessStatus($0.id) == .ship }
-                            .sorted { $0.date > $1.date }
-                    ),
-                    (
-                        label: OrderBusinessStatus.received.descriptionWithPicto,
-                        orders: allOrders
-                            .filter { appController.orderBusinessStatus($0.id) == .received }
-                            .sorted { $0.dateStatusChanged < $1.dateStatusChanged }
-                    ),
-                    (
-                        label: OrderBusinessStatus.inTransit.descriptionWithPicto,
-                        orders: allOrders
-                            .filter { appController.orderBusinessStatus($0.id) == .inTransit && !appController.orderChecklistUnchangedFor30Days($0.id) }
-                            .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
-                    ),
-                    (
-                        label: "􀐫 Recently closed",
-                        orders: allOrders
-                            .filter { appController.orderBusinessStatus($0.id) == .closed && !appController.orderChecklistUnchangedFor30Days($0.id) }
-                            .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
-                    ),
-                ]
-                ForEach(sections, id: \.label) { s in
-                    
-                    section(header: s.label, orders: s.orders)
-                }
+                section(
+                    header: "􁁿 In transit for 30+ days",
+                    orders: allOrders
+                        .filter { appController.orderBusinessStatus($0.id) == .inTransit && appController.orderChecklistUnchangedFor30Days($0.id) }
+                        .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
+                )
+                section(
+                    header: OrderBusinessStatus.giveFeedback.descriptionWithPicto,
+                    orders: allOrders
+                        .filter { appController.orderBusinessStatus($0.id) == .giveFeedback }
+                        .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
+                )
+                section(
+                    header: OrderBusinessStatus.validatePayment.descriptionWithPicto,
+                    orders: allOrders
+                        .filter { appController.orderBusinessStatus($0.id) == .validatePayment }
+                        .sorted { $0.date > $1.date }
+                )
+                section(
+                    header: OrderBusinessStatus.pickAndPack.descriptionWithPicto,
+                    orders: allOrders
+                        .filter { appController.orderBusinessStatus($0.id) == .pickAndPack }
+                        .sorted { $0.lots < $1.lots }
+                )
+                section(
+                    header: OrderBusinessStatus.ship.descriptionWithPicto,
+                    orders: allOrders
+                        .filter { appController.orderBusinessStatus($0.id) == .ship }
+                        .sorted { $0.date > $1.date }
+                )
+                section(
+                    header: OrderBusinessStatus.received.descriptionWithPicto,
+                    orders: allOrders
+                        .filter { appController.orderBusinessStatus($0.id) == .received }
+                        .sorted { $0.dateStatusChanged < $1.dateStatusChanged }
+                )
+                section(
+                    header: OrderBusinessStatus.inTransit.descriptionWithPicto,
+                    orders: allOrders
+                        .filter { appController.orderBusinessStatus($0.id) == .inTransit && !appController.orderChecklistUnchangedFor30Days($0.id) }
+                        .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
+                )
+                section(
+                    header: "􀐫 Recently closed",
+                    orders: allOrders
+                        .filter { appController.orderBusinessStatus($0.id) == .closed && !appController.orderChecklistUnchangedFor30Days($0.id) }
+                        .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
+                )
                 
                 let closedOrders = allOrders
-                    .filter {
-                        appController.orderBusinessStatus($0.id) == .closed && appController.orderChecklistUnchangedFor30Days($0.id)
-                    }
+                    .filter { appController.orderBusinessStatus($0.id) == .closed && appController.orderChecklistUnchangedFor30Days($0.id) }
                     .sorted { $0.dateStatusChanged > $1.dateStatusChanged }
                 
                 ForEach(closedOrders.grouppedByMonth, id: \.month) { item in
-                    
                     section(header: "􀤟 \(item.month)", orders: item.elements)
                 }
             }
