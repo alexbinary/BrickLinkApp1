ab9c78e38d2340fad220780d620b50170e3cc727
Alexandre Bintz <alexandre@bintz.io>
2025-02-27 12:11:56 +0100

    experiment: speech recognition: wip

--- Experimental1/Src/App.swift
+++ Experimental1/Src/App.swift
@@ -10,7 +10,7 @@ struct Experimental1: App {
     var body: some Scene {
 
         WindowGroup {
-            ObservationRootView()
+            SpeechRecognitionRootView()
         }
     }
 }
---
+++ Experimental1/Src/SpeechRecognition/SpeechRecognitionRootView.swift
@@ -0,0 +1,284 @@
+
+import SwiftUI
+import Speech
+
+
+
+struct SpeechRecognitionRootView: View {
+
+    
+    @State var controller = SpeechRecognitionController()
+    
+    
+    var body: some View {
+
+        VStack(alignment: .leading) {
+            
+            LabeledContent("Microphone authorized") {
+                if let authorized = controller.microphoneAuthorized {
+                    Text(authorized ? "Yes" : "No")
+                } else {
+                    Text("undetermined")
+                }
+            }
+            LabeledContent("Speech recognition available") {
+                if let available = controller.speechRecognitionAvailable {
+                    Text(available ? "Yes" : "No")
+                } else {
+                    Text("undetermined")
+                }
+            }
+            LabeledContent("Speech recognition authorized") {
+                if let authorized = controller.speechRecognitionAuthorized {
+                    Text(authorized ? "Yes" : "No")
+                } else {
+                    Text("undetermined")
+                }
+            }
+            Group {
+                LabeledContent("Listening") {
+                    Text(controller.listening ? "Yes" : "No")
+                }
+                LabeledContent("Text") {
+                    Text(controller.recognizedText ?? "")
+                }
+                
+                if controller.listening {
+                    Button("Stop") {
+                        controller.stop()
+                    }
+                } else {
+                    Button("Start") {
+                        Task {
+                            await controller.start()
+                        }
+                    }
+                }
+            }
+            .disabled({
+                if let available = controller.speechRecognitionAvailable, available == false {
+                    return true
+                }
+                if let authorized = controller.speechRecognitionAuthorized, authorized == false {
+                    return true
+                }
+                return false
+            }())
+        }
+    }
+}
+
+
+
+#Preview {
+    SpeechRecognitionRootView()
+}
+
+
+
+@Observable
+class SpeechRecognitionController: NSObject, SFSpeechRecognizerDelegate {
+    
+    var microphoneAuthorized: Bool?
+    
+    var speechRecognitionAvailable: Bool?
+    var speechRecognitionAuthorized: Bool?
+    
+    var listening = false
+    var recognizedText: String?
+    
+    
+    var audioEngine: AVAudioEngine!
+    var inputNode: AVAudioInputNode!
+    
+    var speechRecognizer: SFSpeechRecognizer!
+    var recognitionRequest: SFSpeechAudioBufferRecognitionRequest!
+    var recognitionTask: SFSpeechRecognitionTask!
+    
+    
+    override init() {
+        super.init()
+        
+        speechRecognizer = SFSpeechRecognizer()
+        guard let speechRecognizer = speechRecognizer else { fatalError("Unable to created a SFSpeechRecognizer object") }
+        speechRecognizer.delegate = self
+        
+        updateMicrophoneAuthorisationStatus()
+        updateSpeechRecognitionAuthorisationStatus()
+    }
+    
+    
+    func start() async {
+        
+        if microphoneAuthorisationStatus != .authorized {
+            await requestMicrophoneAuthorisation()
+        }
+        updateMicrophoneAuthorisationStatus()
+        let microphoneStatus = microphoneAuthorisationStatus
+        guard microphoneStatus == .authorized else {
+            print("Microphone not authorized (\(microphoneStatus))")
+            return
+        }
+        print("Microphone authorized (\(microphoneStatus))")
+        
+        let speechRecognitonStatus = await requestSpeechRecognitionAuthorisation()
+        updateSpeechRecognitionAuthorisationStatus()
+        guard speechRecognitonStatus == .authorized else {
+            print("Speech recognition not authorized (\(speechRecognitonStatus))")
+            return
+        }
+        print("Speech recognition authorized (\(speechRecognitonStatus))")
+        
+        do {
+            try initAudio()
+        } catch {
+            print("Failed to init audio: \(error)")
+        }
+        print("Audio init success")
+        
+        startRecognition()
+        print("Recognition active")
+        
+        listening = true
+    }
+    
+    
+    func stop() {
+        
+        recognitionTask.cancel()
+        stopAudio()
+        
+        listening = false
+    }
+    
+    
+    var speechRecognitionAuthorisationStatus: SFSpeechRecognizerAuthorizationStatus {
+        
+        SFSpeechRecognizer.authorizationStatus()
+    }
+    
+    
+    func updateSpeechRecognitionAuthorisationStatus() {
+        
+        speechRecognitionAuthorized = speechRecognitionAuthorisationStatus == .authorized
+    }
+    
+    
+    func requestSpeechRecognitionAuthorisation() async -> SFSpeechRecognizerAuthorizationStatus {
+        
+        await withCheckedContinuation { continuation in
+            SFSpeechRecognizer.requestAuthorization { status in
+                continuation.resume(returning: status)
+            }
+        }
+    }
+
+    
+    var microphoneAuthorisationStatus: AVAuthorizationStatus {
+        
+        AVCaptureDevice.authorizationStatus(for: .audio)
+    }
+    
+    
+    func updateMicrophoneAuthorisationStatus() {
+        
+        microphoneAuthorized = microphoneAuthorisationStatus == .authorized
+    }
+    
+    
+    func requestMicrophoneAuthorisation() async {
+        
+        await withCheckedContinuation { continuation in
+            AVCaptureDevice.requestAccess(for: .audio) { granted in
+                continuation.resume()
+            }
+        }
+    }
+    
+    
+    func initAudio() throws {
+        
+        audioEngine = AVAudioEngine()
+        inputNode = audioEngine.inputNode
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+    }
+    
+    
+    func stopAudio() {
+        
+        audioEngine.stop()
+        inputNode.removeTap(onBus: 0)
+    }
+    
+    
+    func startRecognition() {
+        
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, time) in
+            self.recognitionRequest?.append(buffer)
+        }
+
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            
+            if let error = error {
+                
+                self.stopAudio()
+                print("Recognition error: \(error)")
+                
+            } else if let result = result {
+                
+                let transcribedText = result.bestTranscription.formattedString
+                print("Transcribed: \(transcribedText)")
+                self.recognizedText = transcribedText
+                if result.isFinal {
+                    self.stopAudio()
+                    print("Recognition complete")
+                }
+            }
+        }
+    }
+    
+    
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        
+        if available {
+            print("Speech recognition is available")
+        } else {
+            print("Speech recognition is not available")
+        }
+        
+        self.speechRecognitionAvailable = available
+    }
+}
+
+
+
+extension SFSpeechRecognizerAuthorizationStatus: @retroactive CustomStringConvertible {
+    
+    public var description: String {
+        
+        switch self {
+            
+        case .notDetermined:
+            "notDetermined"
+        
+        case .denied:
+            "denied"
+        
+        case .restricted:
+            "restricted"
+        
+        case .authorized:
+            "authorized"
+        
+        @unknown default:
+            "unknown"
+        }
+    }
+}
