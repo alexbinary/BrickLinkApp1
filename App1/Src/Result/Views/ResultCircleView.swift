04618889d846cead1f0f7571e2dd1996b3454b66
Alexandre Bintz <alexandre@bintz.io>
2025-03-17 10:39:46 +0100

    refactor ResultCircleView

--- App1/Src/Result/Views/ResultCircleView.swift
+++ App1/Src/Result/Views/ResultCircleView.swift
@@ -1,6 +1,6 @@
 
 import SwiftUI
-import Charts
+import Percentage
 
 
 
@@ -14,52 +14,40 @@ struct ResultCircleView: View {
     let innerCircleSize: CGFloat
     let outerCircleSize: CGFloat
     
+    
+    let lineWidthTrack: CGFloat = 4
+    let lineWidthValue: CGFloat = 6
+    let circleBaseTrim: CGFloat = 0.95
+    let circleRotation: Angle = .radians(-3 * .pi/4)
+    
     @State var animateCircles = false
     
     
     var body: some View {
         
-        let ratios: (income: CGFloat, expense: CGFloat) = {
+        let ratios: (income: Percentage, expense: Percentage) = {
             
-            let income = CGFloat(income)
-            let expense = CGFloat(expense)
+            let income = Double(income)
+            let expense = Double(expense)
             
             if income == 0 || expense == 0 {
                 return (income: 0, expense: 0)
             }
             
             if income > expense {
-                return (income: 1.0, expense: expense/income)
+                return (income: 100%, expense: Percentage(fraction: expense/income))
             } else {
-                return (income: income/expense, expense: 1.0)
+                return (income: Percentage(fraction: income/expense), expense: 100%)
             }
         }()
         
         ZStack(alignment: .center) {
             
-            Circle()
-                .trim(from: 0, to: 0.95)
-                .rotation(.radians(-3 * .pi/4))
-                .stroke(red.opacity(0.1), style: .init(lineWidth: 4, lineCap: .round))
-                .frame(width: innerCircleSize, height: innerCircleSize)
+            circle(size: outerCircleSize, lineWidth: lineWidthTrack, value: 100%, color: green.opacity(0.1))
+            circle(size: outerCircleSize, lineWidth: lineWidthValue, value: ratios.income, color: green)
             
-            Circle()
-                .trim(from: 0, to: 0.95)
-                .rotation(.radians(-3 * .pi/4))
-                .stroke(green.opacity(0.1), style: .init(lineWidth: 4, lineCap: .round))
-                .frame(width: outerCircleSize, height: outerCircleSize)
-            
-            Circle()
-                .trim(from: 0, to: 0.95 * ratios.expense)
-                .rotation(.radians(-3 * .pi/4))
-                .stroke(red, style: .init(lineWidth: 6, lineCap: .round))
-                .frame(width: innerCircleSize, height: innerCircleSize)
-            
-            Circle()
-                .trim(from: 0, to: 0.95 * ratios.income)
-                .rotation(.radians(-3 * .pi/4))
-                .stroke(green, style: .init(lineWidth: 6, lineCap: .round))
-                .frame(width: outerCircleSize, height: outerCircleSize)
+            circle(size: innerCircleSize, lineWidth: lineWidthTrack, value: 100%, color: red.opacity(0.1))
+            circle(size: innerCircleSize, lineWidth: lineWidthValue, value: ratios.expense, color: red)
             
             if !profitMargin.isNaN {
                 VStack {
@@ -70,10 +58,20 @@ struct ResultCircleView: View {
             }
         }
         .animation(.easeOut(duration: 0.2), value: animateCircles ? [ratios.income, ratios.expense] : nil)
-        .onAppear {
-            DispatchQueue.main.async {
-                self.animateCircles = true
-            }
-        }
+        .onAppear { DispatchQueue.main.async {
+            self.animateCircles = true
+        }}
+    }
+    
+    
+    @ViewBuilder
+    func circle(size: CGFloat, lineWidth: CGFloat, value: Percentage, color: Color) -> some View {
+        
+        Circle()
+            .trim(from: 0, to: circleBaseTrim * value.fraction)
+            .rotation(circleRotation)
+            .stroke(color, style: .init(lineWidth: lineWidth, lineCap: .round))
+            .frame(width: size, height: size)
+        
     }
 }
