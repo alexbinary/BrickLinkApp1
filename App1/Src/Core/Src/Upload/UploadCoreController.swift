5f8dad86d276cf3a48a81b6637c89ae062ad4cab
Alexandre Bintz <alexandre@bintz.io>
2025-03-26 16:16:35 +0100

    core UploadCoreController

--- App1/Src/Core/Src/Upload/UploadCoreController.swift
+++ App1/Src/Core/Src/Upload/UploadCoreController.swift
@@ -1,16 +1,15 @@
 
 import Foundation
-import Core
 
 
 
-class UploadCoreController {
+public class UploadCoreController {
     
 
     private let dataStore: DataStore
     
     
-    init(_ dataStore: DataStore) {
+    public init(_ dataStore: DataStore) {
         
         self.dataStore = dataStore
     }
