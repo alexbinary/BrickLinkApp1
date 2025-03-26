d4766221a7d9dfbc26c4c8e6cc9f9e3d0c87a74c
Alexandre Bintz <alexandre@bintz.io>
2025-03-26 16:19:20 +0100

    core ShippingCoreController

--- App1/Src/Core/Src/Shipping/ShippingCoreController.swift
+++ App1/Src/Core/Src/Shipping/ShippingCoreController.swift
@@ -1,16 +1,15 @@
 
 import Foundation
-import Core
 
 
 
-class ShippingCoreController {
+public class ShippingCoreController {
     
     
     private let dataStore: DataStore
     
     
-    init(_ dataStore: DataStore) {
+    public init(_ dataStore: DataStore) {
         
         self.dataStore = dataStore
     }
