6bb640b880c3b5858aa6e821605ccbf4f6372aba
Alexandre Bintz <alexandre@bintz.io>
2025-03-26 16:55:59 +0100

    core env

---
+++ App1/Src/Core/Src/env.swift
@@ -0,0 +1,97 @@
+
+import Foundation
+
+
+
+public typealias Env = (
+    
+    catalog: Catalog,
+    
+    stores: (
+        
+        inventory: InventoryUserStore,
+        upload: UploadUserStore,
+
+        order: OrderUserStore,
+        picking: PickingUserStore,
+        shipping: ShippingUserStore,
+        tracking: TrackingUserStore,
+        feedback: FeedbackUserStore,
+        refund: RefundUserStore,
+
+        transaction: TransactionUserStore,
+        result: ResultUserStore
+    )
+)
+
+
+
+@MainActor
+public func createEnv(brickLinkCredentials: BrickLinkAPICredentials, debug: Debug) -> Env {
+    
+    let dataStore: DataStore = {
+        let path = FileManager.default.currentDirectoryPath.appending("/data/data.json5")
+        return DataStore(dataFileUrl: URL(fileURLWithPath: path))
+    }()
+    
+    let brickLinkAPIClient = BrickLinkAPIClient(withCredentials: brickLinkCredentials, debug: debug)
+    
+    // Core controllers
+    
+    let catalog = Catalog(dataStore, brickLinkAPIClient)
+    
+    let inventoryCoreController = InventoryCoreController(dataStore, brickLinkAPIClient)
+    let uploadCoreController = UploadCoreController(dataStore)
+    
+    let orderCoreController = OrderCoreController(dataStore, brickLinkAPIClient)
+    let pickingCoreController = PickingCoreController(dataStore)
+    let shippingCoreController = ShippingCoreController(dataStore)
+    let trackingCoreController = TrackingCoreController(dataStore, debug)
+    let feedbackCoreController = FeedbackCoreController(dataStore, brickLinkAPIClient)
+    let refundCoreController = RefundCoreController(dataStore)
+    let transactionCoreController = TransactionCoreController(dataStore)
+    
+    let trackingMiddleController = TrackingMiddleController(trackingCoreController, orderCoreController)
+    let feedbackPostController = FeedbackPostController(feedbackCoreController, orderCoreController)
+    
+    let pickingProgressCoreController = PickingProgressCoreController(pickingCoreController, orderCoreController)
+    let orderChecklistCoreController = OrderChecklistCoreController(orderCoreController, pickingCoreController, shippingCoreController, feedbackCoreController, transactionCoreController, trackingMiddleController, pickingProgressCoreController)
+    let orderMacroStatusCoreController = OrderMacroStatusCoreController(orderCoreController, orderChecklistCoreController)
+    let stockCoreController = StockCoreController(inventoryCoreController, pickingCoreController, orderCoreController, orderMacroStatusCoreController)
+    
+    // User Stores
+    
+    let inventoryUserStore = InventoryUserStore(inventoryCoreController, stockCoreController)
+    let uploadUserStore = UploadUserStore(uploadCoreController, inventoryCoreController, catalog)
+    
+    let orderUserStore = OrderUserStore(orderCoreController, orderChecklistCoreController, orderMacroStatusCoreController, pickingProgressCoreController, trackingMiddleController, feedbackCoreController, feedbackPostController)
+    let pickingUserStore = PickingUserStore(pickingCoreController, pickingProgressCoreController, orderCoreController)
+    let shippingUserStore = ShippingUserStore(shippingCoreController, orderCoreController)
+    let trackingUserStore = TrackingUserStore(trackingMiddleController)
+    let feedbackUserStore = FeedbackUserStore(feedbackCoreController, feedbackPostController)
+    let refundUserStore = RefundUserStore(refundCoreController)
+    
+    let transactionUserStore = TransactionUserStore(transactionCoreController)
+    let resultUserStore = ResultUserStore(orderCoreController, shippingCoreController, refundCoreController, transactionCoreController)
+    
+    return (
+        
+        catalog: catalog,
+        
+        stores: (
+            
+            inventory: inventoryUserStore,
+            upload: uploadUserStore,
+            
+            order: orderUserStore,
+            picking: pickingUserStore,
+            shipping: shippingUserStore,
+            tracking: trackingUserStore,
+            feedback: feedbackUserStore,
+            refund: refundUserStore,
+            
+            transaction: transactionUserStore,
+            result: resultUserStore
+        )
+    )
+}
